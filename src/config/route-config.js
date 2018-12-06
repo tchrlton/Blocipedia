bbb2de97768f3b6f006f1cc3c4fe8ff7aedafdfa
Thomas Charlton <tchrlton1@gmail.com>
2018-12-06 02:46:37 -0800

    debugging create request

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -2,7 +2,11 @@ module.exports = {
   init(app){
     const staticRoutes = require("../routes/static");
     const userRoutes = require("../routes/users");
+    const wikiRoutes = require("../routes/wikis");
+
     app.use(staticRoutes);
     app.use(userRoutes);
+    app.use(wikiRoutes);
   }
 }
+
