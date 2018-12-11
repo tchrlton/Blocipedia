e9fd627de7ef53e7d4c0103476a5c4ceea311a12
Thomas Charlton <tchrlton1@gmail.com>
2018-12-10 16:41:37 -0800

    forgot to delete one of the routes

--- src/routes/collaborators.js
+++ src/routes/collaborators.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const collaboratorController = require("../controllers/collaboratorController");
 const User = require("../../src/db/models").User;
 
-router.get("/wikis/:wikiId/collaborators", collaboratorController.show);
 router.post("/wikis/:wikiId/collaborators/create", collaboratorController.create);
 router.post("/wikis/:wikiId/collaborators/delete", collaboratorController.delete);
 
